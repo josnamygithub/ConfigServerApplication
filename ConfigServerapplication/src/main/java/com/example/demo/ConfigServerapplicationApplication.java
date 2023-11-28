d9b6116672177af95beed7c2a1d27787ec043e1e
josnamygithub <131941980+josnamygithub@users.noreply.github.com>
2023-11-28 10:58:36 +0530

    Add files via upload

---
+++ ConfigServerapplication/src/main/java/com/example/demo/ConfigServerapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerapplicationApplication.class, args);
+	}
+
+}
